18077b718f953f347c1bcd969293d19ecfa14647
Eman Samkri <Eman.Jameel.jed.k2@tuwaiq.edu.sa>
2021-11-02 05:52:21 +0300

    nice

--- app/src/main/java/com/shady/hw_week04_day01_recyclerviewiml/MainActivity.kt
+++ app/src/main/java/com/shady/hw_week04_day01_recyclerviewiml/MainActivity.kt
@@ -2,10 +2,57 @@ package com.shady.hw_week04_day01_recyclerviewiml
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+private lateinit var contReyclerView: RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    val contacts = mutableListOf<Contact>()
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val contactList = mutableListOf<Contact>()
+        for (i in 0 until 100){
+            val contact =Contact()
+            contact.name = "Eman $i"
+            contact.phonNum = 1234567891
+            contactList.add(contact)
+        }
+
+
+        contReyclerView = findViewById(R.id.recyclerViewId)
+
+        val linearLayoutManger = LinearLayoutManager(this)
+        contReyclerView.layoutManager = linearLayoutManger
+
+        val adapter = RycAdapter(contactList)
+        contReyclerView.adapter = adapter
+
+
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
